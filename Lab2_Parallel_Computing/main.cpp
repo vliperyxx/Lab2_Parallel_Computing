1c6d3568eec1212ae43f8cec8ac7e5c4462d034d
vlady <vladysha1021@gmail.com>
2025-03-09 10:46:47 -0400

    Added single-threaded function

--- Lab2_Parallel_Computing/main.cpp
+++ Lab2_Parallel_Computing/main.cpp
@@ -13,4 +13,30 @@ public:
             arr[i] = rand() % 1001; 
         }
     }
+
+    static void findCountAndMaxGreaterThan20(const int arr[], int size, int& count, int& maxValue) {
+        count = 0;
+        maxValue = INT_MIN;
+        for (int i = 0; i < size; i++) {
+            if (arr[i] > 20) {
+                count++;
+                if (arr[i] > maxValue) {
+                    maxValue = arr[i];
+                }
+            }
+        }
+    }
 };
+
+int main() {
+    int size;
+    std::cout << "Enter size of array: ";
+    std::cin >> size;
+
+    int* array = new int[size];
+
+    ArrayOperations::fillArray(array, size);
+
+    delete[] array;
+    return 0;
+}
